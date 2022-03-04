500c4a5221238969a8c10c1d779908e6dd9d385a
ryo-is <ryosuke.izumi62@gmail.com>
2022-03-04 10:58:43 +0900

    fisrt commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import fs from "fs";
+import parseMD from "parse-md";
+
+const fileContents = fs.readFileSync("test.md", "utf8");
+const { metadata, content } = parseMD(fileContents);
+
+console.log("metadata", metadata);
+console.log("content", content);
