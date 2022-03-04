33b37ac2e2ebe22a026e51a003fb264bfdacbc37
ryo-is <ryosuke.izumi62@gmail.com>
2022-03-04 11:07:20 +0900

    fix

--- index.ts
+++ index.ts
@@ -5,4 +5,5 @@ const fileContents = fs.readFileSync("test.md", "utf8");
 const { metadata, content } = parseMD(fileContents);
 
 console.log("metadata", metadata);
+
 console.log("content", content);
